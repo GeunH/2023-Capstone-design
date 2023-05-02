c4ebea232f2e68deb140175e28181ec39a6bdb32
GeunH <114638557+GeunH@users.noreply.github.com>
2023-05-02 15:32:30 +0900

    Update my_three.js

--- my_three.js
+++ my_three.js
@@ -4,8 +4,9 @@ import { OBJLoader } from "https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm
 
 const w = window.innerWidth;
 const h = window.innerHeight;
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
+var scene = new THREE.Scene();
+scene.background = new THREE.Color(0x000000);
+const camera = new THREE.PerspectiveCamera(75, w / h , 0.1, 1000);
 camera.position.z = 5;
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(w, h);
@@ -15,28 +16,52 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.update();
 
 function init (geometry) { 
-const material = new THREE.MeshMatcapMaterial({
+  const material = new THREE.MeshMatcapMaterial({
     // color 속성 대신 vertexColors 속성을 사용하여 Vertex Colors를 활성화
     vertexColors: THREE.VertexColors,
-});
-const mesh = new THREE.Mesh(geometry, material);
+    
+  });
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
 
-scene.add(mesh);
-
-function animate() {
+  function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
-}
-animate();
+  }
+  animate();
 }
 
-const loader = new OBJLoader();
-loader.load("pizhon_256.obj", (obj) => {
+var f_name = "";
+initScene();
+
+function initScene() {
+  while (scene.children.length > 0) {
+    scene.remove(scene.children[0]);
+  }
+  const loader = new OBJLoader();
+  loader.load(f_name, (obj) => {
     init(obj.children[0].geometry);
+  });
+}
+
+const radioButtons = document.querySelectorAll('input[type="radio"][name="file"]');
+let selectedValue;
+
+radioButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    selectedValue = button.value;
+    if( f_name != selectedValue)
+    {
+      f_name = selectedValue;
+      initScene();
+    }
+  });
 });
+
 function handleWindowResize () {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
+
 window.addEventListener('resize', handleWindowResize, false);
