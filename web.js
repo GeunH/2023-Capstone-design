aa265095bcbac52582392a6b4b05e48a6826e85a
GeunH <114638557+GeunH@users.noreply.github.com>
2023-05-26 17:52:43 +0900

    Update web.js

--- web.js
+++ web.js
@@ -1,40 +1,51 @@
-const http = require('http');
+const express = require('express');
+const cors = require('cors');
+const { exec } = require('child_process');
+const app = express();
 const fs = require('fs');
 const path = require('path');
+app.use(cors());
+app.use('/models', express.static('models'));
+app.use(express.json());
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'webb.html'));
+});
 
-const server = http.createServer((req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Cache-Control', 'max-age=60');
+app.get('/load_obj.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'load_obj.html'));
+});
 
-  let filePath;
-  if (req.url === '/') {
-    filePath = path.join(__dirname, 'web.html');
-  } else {
-    filePath = path.join(__dirname, req.url);
-  }
-  
-  fs.readFile(filePath, 'utf-8', (err, data) => {
+app.get('/good.js', (req, res) => {
+  res.sendFile(path.join(__dirname, 'good.js'));
+});
+
+app.get('/files', (req, res) => {
+  const folderPath = 'C:/Users/ESE/Desktop/capstone/models';
+  fs.readdir(folderPath, (err, files) => {
     if (err) {
-      res.statusCode = 500;
-      res.end('Internal Server Error');
-    } else {
-      let contentType = 'text/plain';
-      if (filePath.endsWith('.html')) {
-        contentType = 'text/html';
-      } else if (filePath.endsWith('.js')) {
-        contentType = 'application/javascript';
-      }
-
-      res.writeHead(200, {
-        'Content-Type': contentType
-      });
-      res.end(data);
+      console.error(err);
+      return res.status(500).send('Internal Server Error');
     }
+    res.json(files);
   });
 });
 
-server.listen(3000, () => {
-  console.log('Server running on port 3000');
+
+app.post('/execute-script', (req, res) => {
+  const fileName = req.body.fileName;
+  const scriptPath = `C:/Users/ESE/Desktop/capstone/models`;
+  const command = `cd "${scriptPath}" && instant-ngp ${fileName}`;
+  console.log(`Script output: ${fileName}`);
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error executing script: ${error.message}`);
+      return res.status(500).send('Error executing script');
+    }
+    console.log(`Script output: ${stdout} and ${fileName}`);
+
+  });
+});
+const PORT = 3050;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
